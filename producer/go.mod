273ece8b742908279157452036c9d8d8f4d2f874
Mojtaba Michael <mojimich2015@gmail.com>
2025-03-02 21:19:33 +0300

    Renamed producer and consumer

--- producer/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/mjmichael73/productproducer
-
-go 1.23.4
-
-require github.com/gorilla/mux v1.8.1 // indirect
---
+++ server/pkg/router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mjmichael73/productproducer/pkg/handler"
+)
+
+func InitializeRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/products", handler.GetProducts).Methods("GET")
+	router.HandleFunc("/products/{id}", handler.GetProduct).Methods("GET")
+	router.HandleFunc("/products", handler.CreateProduct).Methods("POST")
+	router.HandleFunc("/products/{id}", handler.UpdateProduct).Methods("PUT")
+	router.HandleFunc("/products/{id}", handler.DeleteProduct).Methods("DELETE")
+	return router
+}
